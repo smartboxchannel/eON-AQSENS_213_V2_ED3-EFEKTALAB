a0e4fe1afb0d4d316c42fcb3dfd02cf04e1c5a2d
Andrew Lamchenko <31867646+smartboxchannel@users.noreply.github.com>
2021-05-09 17:32:06 +0300

    V 0.93

---
+++ CODE/ARDUINO/aConfig.h
@@ -0,0 +1,38 @@
+
+// ###################   Mini air quality and wither station with electronic ink display 2.13 Inch | nRF52   ############### //
+//                                                                                                                           //
+//        @filename   :   MWS213_V2R3_ED3.ino                                                                                //
+//        @brief en   :   Wireless, battery-operated air quality(VOC Sensor SGP40), temperature,humidity and pressure        //
+//                        sensor(BME280) with electronic ink display(Good Display GDEH0213B72, GDEH0213B73, Waveshare V2).   //
+//                        Works on SOC nRF52.                                                                                //
+//        @brief ru   :   Беcпроводной, батарейный датчик качества воздуха (ЛОС сенсорс SGP40), температуры, влажности       //
+//                        и давления(BME280) с дисплеем на электронных чернилах(Good Display GDEH0213B72, GDEH0213B73,       //
+//                        Waveshare V2).                                                                                     //
+//                        Работает на nRF52832, nRF52840.                                                                    //
+//                                                                                                                           //
+//        @author     :   Andrew Lamchenko aka Berk                                                                          //
+//                                                                                                                           //
+//        Copyright (C) 2021, EFEKTALAB                                                                                      //
+//        Copyright (c) 2020, Sensirion AG                                                                                   //
+//        Copyright (c) 2020, Bosch Sensortec GmbH. All rights reserved.                                                     //
+//        Copyright (c) 2014-2015, Arduino LLC.  All right reserved.                                                         //
+//        Copyright (c) 2016, Arduino Srl.  All right reserved.                                                              //
+//        Copyright (c) 2017, Sensnology AB. All right reserved.                                                             //
+//        Copyright (C) 2020, Waveshare                                                                                      //
+//                                                                                                                           //
+// ######################################################################################################################### //
+
+
+//#define MY_DEBUG
+#define LANG_RU // If this is not used the English localization will be displayed.
+#define SN "EFEKTA Air Quality Sensor"
+#define SV "0.93"
+#define MY_RADIO_NRF5_ESB
+#define MY_NRF5_ESB_PA_LEVEL (0x8UL)
+//#define MY_PASSIVE_NODE
+//#define MY_NODE_ID 151
+//#define MY_NRF5_ESB_MODE (NRF5_1MBPS)
+#define MY_NRF5_ESB_MODE (NRF5_250KBPS)
+#define ESPECIALLY
+#define SEND_RESET_REASON
+#define MY_RESET_REASON_TEXT
